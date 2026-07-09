86dda6b6856683fc3e29f2d8fea6f502906e3f07
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-07-09 17:24:02 -0300

    Adiciona DbSets comentados para entidades do DbContext

--- EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
+++ EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
@@ -6,6 +6,13 @@ namespace EscolaDeCursos.Infra.Compartilhado.Orm;
 public sealed class EscolaDeCursosDbContext(
     DbContextOptions<EscolaDeCursosDbContext> options) : DbContext(options)
 {
+    // public DbSet<Curso> Cursos => Set<Curso>();
+    // public DbSet<Instrutor> Instrutores => Set<Instrutor>();
+    // public DbSet<ModuloCurso> ModulosDoCurso => Set<ModuloCurso>();
+    // public DbSet<Categoria> Categorias => Set<Categoria>();
+    // public DbSet<Turma> Turmas => Set<Turma>();
+    // public DbSet<Matricula> Matriculas => Set<Matricula>();
+    // public DbSet<Aluno> Alunos => Set<Aluno>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         Assembly assembly = typeof(EscolaDeCursosDbContext).Assembly;
