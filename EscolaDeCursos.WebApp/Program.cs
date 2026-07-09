1741916ebf88c23a7760c7ddad6c03168bb59799
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-07-09 14:14:42 -0300

    Adiciona projeto inicial

---
+++ EscolaDeCursos.WebApp/Program.cs
@@ -0,0 +1,20 @@
+using EscolaDeCursos.Aplicacao;
+using EscolaDeCursos.Infra;
+using EscolaDeCursos.WebApp.Compartilhado;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do container de injeção de dependência
+
+builder.Services.AddInfraRepositories(builder.Configuration, builder.Logging);
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddPresentationConfig(builder.Configuration);
+
+var app = builder.Build();
+
+// Middlewares de roteamento
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+// Execução do Servidor
+app.Run();
